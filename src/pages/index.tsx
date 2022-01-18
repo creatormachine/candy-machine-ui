5da9f95e4f979ce5f3cafc17c98f0ce22f35a542
Stanley Yang <me@stanleycyang.com>
2022-01-17 17:26:00 -0800

    Set up basic routing, tailwind, removed unnecessary files

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ["Roboto Mono", "sans-serif"],
+      display: ["Roboto Mono", "sans-serif"],
+      body: ["Roboto Mono", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
